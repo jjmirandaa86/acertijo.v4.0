27bbde9bbcbb2d1f6baeeaf4071f533477ab7fd4
Jeff Miranda <jjmirandaa86@gmail.com>
2024-08-25 19:11:50 +1000

    modify time photo main, dark carousels

--- src/components/layout/Imagenes.js
+++ src/components/layout/Imagenes.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext from "../../contexts/ThemeContext";
 import { Carousel, Image } from "react-bootstrap";
 
 const Imagenes = () => {
+	const { theme } = useContext(ThemeContext);
+
 	return (
 		<>
-			<Carousel>
+			<Carousel data-bs-theme={theme === "light" ? "dark" : "white"}>
 				<Carousel.Item interval={3000}>
 					<img width={300} src="assets/ima/developer-main.png" alt="First slide" />
 				</Carousel.Item>
-				<Carousel.Item interval={1500}>
+				<Carousel.Item interval={3000}>
 					<Image
 						width={300}
 						src="assets/ima/photos/jeff-happy-light.jpg"
